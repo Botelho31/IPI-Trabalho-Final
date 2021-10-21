880b4469e72cdb5daaf9dc45968024fe4dee984a
Lucas Botelho <lucas.vbotelho83@gmail.com>
2021-10-21 10:30:00 -0300

    refactor: removed encription from embedding function

--- prog.py
+++ prog.py
@@ -5,14 +5,6 @@ from show_image import show_image
 from arnold_transform import arnold_transform
 from write_image import write_image
 
-def bitstring_to_bytes(s):
-    v = int(s, 2)
-    b = bytearray()
-    while v:
-        b.append(v & 0xff)
-        v >>= 8
-    return bytes(b[::-1])
-
 def transform_image_to_bits(img):
   array = img.flatten()
   bytes = array.tobytes()
@@ -87,33 +79,26 @@ def unembed_binary(img, size, pixel_block_size, bit_array_size):
 
 
 def perform_watermark_embedding(img,img_size,logo,logo_size,pixel_block_size):
-  # revisar periodo da transformação de arnold pelo tamanho da imagem
-  enciter = int(logo_size/2) # qualquer numero entre 0 e o periodo
-  deciter = (int(logo_size/16)) * 12 - enciter # o periodo menos o enciter
-  scrambled_logo = arnold_transform(logo, enciter)
 
   # transforma a imagem em uma sequencia de bits
-  logo_bit_array = transform_image_to_bits(scrambled_logo)
+  logo_bit_array = transform_image_to_bits(logo)
 
   # da o embed na imagem original com a logomarca
-  embedded_image = embed_binary(img,img_size,pixel_block_size,logo_bit_array)
+  embedded_image = embed_binary(img, img_size, pixel_block_size, logo_bit_array)
 
   # remove a sequencia de bits da logomarca
-  new_bit_array = unembed_binary(embedded_image,img_size,pixel_block_size,len(logo_bit_array))
+  new_bit_array = unembed_binary(embedded_image, img_size, pixel_block_size, len(logo_bit_array))
 
   # transforma a sequencia de bits em imagem
   new_scrambled_logo = transform_bits_to_image(new_bit_array, logo_size)
 
-  #unscramble_logo
-  unscrambled_logo = arnold_transform(new_scrambled_logo, deciter)
-
-  return (embedded_image,unscrambled_logo)
+  return (embedded_image,new_scrambled_logo)
 
 
 def main():
   img_size = 512
-  logo_size = 64
-  pixel_block_size = 2
+  logo_size = 128
+  pixel_block_size = 1
   img = cv.imread('assets/lena.tif',cv.IMREAD_COLOR)
   img = cv.resize(img,(img_size,img_size),interpolation = cv.INTER_AREA)
 
@@ -123,12 +108,24 @@ def main():
   img_b,img_g,img_r = cv.split(img)
   logo_b,logo_g,logo_r = cv.split(logo)
 
-  (embedded_image_r,unscrambled_logo_r) = perform_watermark_embedding(img_r,img_size,logo_r,logo_size,pixel_block_size)
-  (embedded_image_g,unscrambled_logo_g) = perform_watermark_embedding(img_g,img_size,logo_g,logo_size,pixel_block_size)
-  (embedded_image_b,unscrambled_logo_b) = perform_watermark_embedding(img_b,img_size,logo_b,logo_size,pixel_block_size)
+  # revisar periodo da transformação de arnold pelo tamanho da imagem
+  enciter = int(logo_size/2) # qualquer numero entre 0 e o periodo
+  deciter = (int(logo_size/16)) * 12 - enciter # o periodo menos o enciter
+  scrambled_logo_r = arnold_transform(logo_r, enciter)
+  scrambled_logo_g = arnold_transform(logo_g, enciter)
+  scrambled_logo_b = arnold_transform(logo_b, enciter)
 
-  embedded_image = cv.merge((embedded_image_b,embedded_image_g,embedded_image_r))
-  unscrambled_logo = cv.merge((unscrambled_logo_b,unscrambled_logo_g,unscrambled_logo_r))
+  (embedded_image_r, scrambled_logo_r) = perform_watermark_embedding(img_r, img_size, scrambled_logo_r, logo_size, pixel_block_size)
+  (embedded_image_g, scrambled_logo_g) = perform_watermark_embedding(img_g, img_size, scrambled_logo_g, logo_size, pixel_block_size)
+  (embedded_image_b, scrambled_logo_b) = perform_watermark_embedding(img_b, img_size, scrambled_logo_b, logo_size, pixel_block_size)
+
+  #unscramble_logo
+  unscrambled_logo_r = arnold_transform(scrambled_logo_r, deciter)
+  unscrambled_logo_g = arnold_transform(scrambled_logo_g, deciter)
+  unscrambled_logo_b = arnold_transform(scrambled_logo_b, deciter)
+
+  embedded_image = cv.merge((embedded_image_b, embedded_image_g, embedded_image_r))
+  unscrambled_logo = cv.merge((unscrambled_logo_b, unscrambled_logo_g, unscrambled_logo_r))
 
   write_image('results/',[embedded_image, unscrambled_logo],'png')
   show_image([img, logo, embedded_image, unscrambled_logo])
